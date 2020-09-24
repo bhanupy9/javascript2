c10bb89630cf0f51c22087017c9f5d113b66e237
bhanupy9 <66543130+bhanupy9@users.noreply.github.com>
2020-09-24 10:51:36 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,59 @@
+
+
+
+// function bhanu(){
+//     window.alert("  hiii bhanu welcome to javascript ");
+//     document.getElementById('on').src='bulbon.jpg'; 
+//     document.getElementById('h1').innerHTML='<h1>YES..,IAM LEARNING JAVASCRIPT I WILL COMPLETE IT.</h1>';
+    
+
+// }
+// function prakash(){
+//     document.getElementById('on').src='bulboff.jpg' , 
+//     document.getElementById('h1').innerHTML='iam learning javascript.'
+
+// }
+
+// var x=10;
+// var g="bhanu prakash";
+//variable operators
+// var a="bhanu";
+// var b=1;
+// var c=2;
+// var result=a+b+c;
+// document.write(" <button> The Result is: "+result+" </button> ");
+
+// document.write(result);
+
+//if else control statements
+// var x=54;
+// if (x%2==0) {
+//     document.write(" <h1>EVEN NUMBER </h1> ")
+// }
+// else{
+//     document.write("<h1>ODD NUMBER</h1> ")
+// }
+// // ............
+// if (x>0) {
+//     document.write("POSITIVE NUMBER")
+// }
+// else if (x<0) {
+//     document.write("NEGATIVE NUMBER")
+// }
+// else{
+//     document.write("ZERO NUMBER")
+// }
+// //........................
+// if (x>0) {
+//     document.write("<h1>POSITIVE NUMBER</h1> ")
+//     if (x%2==0) {
+//         document.write("<h1>EVEN NUMBER ALSO</h1> ")
+//     }
+//     else{
+//         document.write("<h1>POSITIVE NUMBER BUT ODD NUMBER</h1> ")
+//     }
+// }
+// else{
+//     document.write(" <h1>NEGATIVE NUMBER</h1> ")
+// } 
+
